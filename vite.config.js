397e9a45ec9e7cb682be01ef59f7e6693b7bbd77
Meer Quais <meerquais017@gmail.com>
2023-12-24 21:36:14 +0500

    made some changes for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: 'src/main.jsx', // Adjust the path accordingly
-      },
-    },
-  },
+  base: process.env.VITE_BASE_PATH || '/deploy_react_app_github_pages_vercel',
 });
